f081d363da25e0b089cd5c83d7ce836a159c1e43
LY <Ly70517@outlook.com>
2023-12-19 19:07:52 +0800

    01

---
+++ 01/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+
+Console.WriteLine("Hello, World!");
+//单行注释
+/*
+ * 多行注释
+ */
+ 
+ //折叠代码块
+
+ #region 标识符的声明
+
+ string a = "我叫大福宝";
+ int b = 2;
+ long c = 1;
+ byte d = 22;
+
+ #endregion
+
+ #region 运算符
+/*
+ * +
+ * -
+ * *
+ * "/"
+ * % 取余运算符，整除后取余数
+ * ++自增运算符，可在数值的前后使用
+ * --自减运算符
+ * 
+ * == 检查两个操作数是否相等
+ * != 不等运算符
+ * >大于
+ * <小于
+ * >=大于等于
+ * <=小于等于
+ *
+ * && 与，两边条件都为真则为真
+ * || 或，两边条件又一个为真，则为真
+ * ! 非，如果条件为真，则为假
+ *
+ * = 赋值运算符 C=A+b,将A+B结果赋值给C
+ * += C+=A,相当于 C=C+A
+ * -= C-=A,相当于C=C-A
+ * *= C*=A,相当于C=C*A
+ * /= C/=A,相当于C=C/A
+ * &= C&=A,相当于C=C&A
+ * 
+*/
+ 
+ 
+
+ #endregion
+ 
+ 
